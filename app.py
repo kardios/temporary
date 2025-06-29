0d7e0db59a6df62e329af8a94a922e863cf393a5
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-06-29 22:41:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ recipient_user_id = os.environ['RECIPIENT_USER_ID']
 bot_token = os.environ['BOT_TOKEN']
 bot = telebot.TeleBot(bot_token)
 
-st.write("**Readhacker** and **Sherwood Lab** have been decommissioned with effect from 30 June 2025.")
+st.write("**Readhacker** and **Sherwood Lab** are decommissioned on 30 June 2025.")
 st.write("With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach **Digital Accelerator Unit** on future capabilities.")
 
 if st.button("Snowfall"): 
