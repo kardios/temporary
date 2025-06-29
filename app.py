8631ddfc75cb0c922f9c902e7d890d886a08e3e3
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-06-29 23:13:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.write("**Readhacker** and **Sherwood Lab** are decommissioned on 30 June 2025
 st.write("With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach **Digital Accelerator Unit** on future capabilities.")
 
 if st.button("Snowfall"): 
-  bot.send_message(chat_id=recipient_user_id, text="Snowfall")  
+  bot.send_message(chat_id=recipient_user_id, text="Readhacker\nSnowfall")  
   st.snow()
 if st.button("Balloons"): 
-  bot.send_message(chat_id=recipient_user_id, text="Balloons")  
+  bot.send_message(chat_id=recipient_user_id, text="Readhacker\nBalloons")  
   st.balloons()
