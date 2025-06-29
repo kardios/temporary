6b71efa64d06c57280032d5fab790ab36e6f0729
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-06-29 22:25:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.write("**Readhacker** and **Sherwood Lab** have been decommissioned with effect from 30 June 2025.")
-st.write("With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach **Digital Accelerator Unit** if you have suggestions on future capabilities. Thank you for your support!")
+st.write("With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach **Digital Accelerator Unit** on future capabilities.")
 if st.button("Snowfall"): st.snow()
 if st.button("Balloons"): st.balloons()
