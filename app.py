0f97410b586acdbfcf75ee4bd2b1377c0e082eac
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-06-29 22:14:16 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.write("**PSA: Readhacker and Sherwood Lab have been decommissioned with effect from 30 June 2025. With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach Digital Accelerator Unit if you have suggestions on future capabilities.**")
+if st.button("Snowfall"): st.snow()
+if st.button("Balloons"): st.balloons()
