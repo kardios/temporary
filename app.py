20325d9388ae249c9075ecde9fa20c91995de44e
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-06-29 22:24:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
-st.write("**Readhacker** and **Sherwood Lab** have been decommissioned with effect from 30 June 2025. With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach **Digital Accelerator Unit** if you have suggestions on future capabilities. Thank you for your support!")
+st.write("**Readhacker** and **Sherwood Lab** have been decommissioned with effect from 30 June 2025.")
+st.write("With widespread availability of AI and dynamic advancements in the AI space, it is timely to explore ways to better contextualise AI for our work. Please approach **Digital Accelerator Unit** if you have suggestions on future capabilities. Thank you for your support!")
 if st.button("Snowfall"): st.snow()
 if st.button("Balloons"): st.balloons()
